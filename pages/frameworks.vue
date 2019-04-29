38f794dc5eb3bdd778ea8d71ec599a3a3bc6c5a9
TAIniko <48309832+TAIniko@users.noreply.github.com>
2019-04-29 17:07:34 +0900

    Update frameworks.vue

--- pages/frameworks.vue
+++ pages/frameworks.vue
@@ -11,6 +11,78 @@
         v-for="framework in frameworks"
         :key="framework.name"
         class="framework-chil"
+<<<<<<< HEAD
+      >
+        <h1>
+          {{ framework.name }}
+        </h1>
+        <div class="frameworkText">
+          {{ framework.text }}
+        </div>
+        <p>{{ framework.level }}</p>
+      </div>
+    </div>
+    <myfooter />
+  </div>
+</template>
+<script>
+import Myfooter from '@/components/Myfooter'
+import Myheader from '~/components/Myheader.vue'
+import Title from '~/components/Title.vue'
+export default {
+  components:{
+    Myheader,
+    Title,
+    Myfooter
+  },
+  data: function(){
+    return{
+      frameworksTitle:'frameworks',
+      frameworks:[
+        {
+          name:'Vue.js',
+          text:'このポートフォリオサイト作成のために公式ドキュメントをよんで勉強しました。',
+          level:'使用歴：1か月'
+        },
+        {
+          name:'Nuxt.js',
+          text:'このポートフォリオサイト作成のために公式ドキュメントをよんで勉強しました。',
+          level:'使用歴：1か月'
+        }
+      ]
+    }
+  },
+  layout:'default',
+  head(){
+    return{
+      title:"frameworks"
+    }
+}
+}
+</script>
+<style>
+.frameworks-wrapper{
+  display:flex;
+  justify-content:space-around;
+  align-content:space-around;
+  height:500px;
+}
+.framework-chil{
+  margin: 0 20px 40px 0;
+  background-color: #f7f7f7;
+  border-radius:6px;
+  width:30%;
+  height:300px;
+  padding:20px;
+  position:relative;
+}
+.frameworks-wrapper p{
+  font-weight:bold;
+  position:absolute;
+  bottom:0;
+}
+</style>
+=======
       >
         <h1>
           {{ framework.name }}
@@ -81,3 +153,4 @@ export default {
   bottom:0;
 }
 </style>
+>>>>>>> 0ed208cdddf68600fd5f6326a0180f272ff11c39
